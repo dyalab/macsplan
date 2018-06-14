08b1c3925692dfdd633f758468f35386b76b84b4
Michael Lesartre <mlesartre@mymail.mine.edu>
2018-06-13 22:35:19 -0600

    changed xmlrpc library

--- website_files/interface.js
+++ website_files/interface.js
@@ -1,19 +1,11 @@
+var xmlrpc = require('xmlrpc')
+var client = xmlrpc.createClient({host: 'localhost', port: 8080, path: '/RPC2'})
 function plan(catalog, student_values){
-	var service = new rpc.ServiceProxy("localhost:8080/RPC2", {
-							 asynchronous: false,   //default: true
-							 sanitize: true,       //default: true
-							 methods: ['plan'],   //default: null (synchronous introspection populates)
-							 protocol: 'XML-RPC', //default: JSON-RPC
-	});
-	console.log(JSON.stringify(student_values))
-	service.plan({
-		params:{catalog:JSON.stringify(catalog), student:JSON.stringify(student_values)},
-		onSuccess:function(message){
-			alert('success');
-			return message;
-		},
-		onException:function(e){
-			return 'could not plan schedule because'+e;
+	client.methodCall('plan', [catalog, student_values], function(error,value){
+		if (error){
+			return error
 		}
-	});
+		return value
+	})
+	alert('this shouldnt happen')
 }
