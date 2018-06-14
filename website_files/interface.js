6f2b67d66447c5f816e4b44e685e2e4fe31ef18a
Michael Lesartre <mlesartre@mymail.mine.edu>
2018-06-13 23:47:39 -0600

    added CORS stuff

--- website_files/interface.js
+++ website_files/interface.js
@@ -2,6 +2,8 @@ function plan(catalog, student_values){
 	var request = new XmlRpcRequest('localhost:8080/RPC2', 'plan');
 	request.addParam(catalog);
 	request.addParam(student_values);
+	request.send_header("Access-Control-Allow-Origin", "*")
+	request.send_header("Access-Control-Allow-Headers","Content-Type")
 	var response = request.send();
 	return response.parseXML();
 	alert('this shouldnt happen')
