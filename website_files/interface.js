6e472839d4d56aa49b23fa6d27ea3c9a44be8470
Michael Lesartre <mlesartre@mymail.mine.edu>
2018-06-14 19:52:12 -0600

    switched back to mimic

--- website_files/interface.js
+++ website_files/interface.js
@@ -1,20 +1,20 @@
 function plan(catalog, student_values){
 	//mimic
 	
-	/*var request = new XmlRpcRequest('localhost:8080/RPC2', 'plan');
+	var request = new XmlRpcRequest('http://localhost:8080/RPC2', 'plan');
 	request.addParam(catalog);
 	request.addParam(student_values);
 	var response = request.send();
 	return response.parseXML();
-	alert('this shouldnt happen')*/
+	alert('this shouldnt happen')
 	
 	
 	//jQuery xmlrpc
-	$.xmlrpc({
+	/*$.xmlrpc({
 		url: 'http://localhost:8080/RPC2',
 		methodName: 'plan',
 		params:[catalog, student_values],
-		success: function(response, status, jqXHR){return response;},
-		error: function(jqXHR, status, error){return error;}
-	});
+		success: function(response, status, jqXHR){alert(response);},
+		error: function(jqXHR, status, error){alert(error);}
+	});*/
 }
