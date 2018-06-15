bc762dad82d8a838d59a47c1772c2d0d32ccd745
Michael Lesartre <mlesartre@mymail.mine.edu>
2018-06-15 14:45:58 -0600

    another change

--- website_files/interface.js
+++ website_files/interface.js
@@ -1,6 +1,6 @@
 function plan(catalog, student_values){
 	var scheduleJson
 	var scheduleJsonDone = $.getJSON("http://macsplan.mines.edu/RPC2?catalog="+catalog+"&student_values="+student_values, function(json){
-    alert(json);
+    console.log(json);
 });
 }
