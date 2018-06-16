a6b4da9f9d7fefd0b7025948c537a0d947319ed7
Michael Lesartre <mlesartre@mines.edu>
2018-06-16 14:51:54 -0600

    add missing quote

--- website_files/interface.js
+++ website_files/interface.js
@@ -1,5 +1,5 @@
 function plan(catalog, student_values, ondone, onfail){
-	$.getJSON("http://macsplan.mines.edu/RPC2, {catalog: catalog, student_values:student_values}, function(json){
+	$.getJSON("http://macsplan.mines.edu/RPC2", {catalog: catalog, student_values:student_values}, function(json){
     ondone(json);
 	}) .fail(function(error){onfail(error)});
 }
