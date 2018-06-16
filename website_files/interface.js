bb13b128fc8c37d5cbebdd12afdeeba0f2b95c50
Michael Lesartre <mlesartre@mines.edu>
2018-06-16 12:54:39 -0600

    changed error handling

--- website_files/interface.js
+++ website_files/interface.js
@@ -1,6 +1,5 @@
-function plan(catalog, student_values, ondone){
-	var scheduleJson
-	var scheduleJsonDone = $.getJSON("http://macsplan.mines.edu/RPC2?catalog="+catalog+"&student_values="+student_values, function(json){
+function plan(catalog, student_values, ondone, onfail){
+	$.getJSON("http://macsplan.mines.edu/RPC2?catalog="+catalog+"&student_values="+student_values, function(json){
     ondone(json);
-});
+	}) .fail(function(error){onfail(error)});
 }
