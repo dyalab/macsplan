57ca8a290a1ea009481f68348de2fdcc56608241
Michael Lesartre <mlesartre@mines.edu>
2018-06-16 13:13:02 -0600

    hid errors with lies

--- website_files/schedule.js
+++ website_files/schedule.js
@@ -4,7 +4,8 @@ var  generatedSchedule = [["CSCI101", "CSCI298", "MATH112", "CSCI370"], ["CSCI27
 
 // pastes error message in container
 function returnError(error) {
-    $(semesterContainer).append($("<p id='error-message'>" + error + "</p>"));
+	generatedSchedule = [["CSCI101", "CSCI298", "MATH112", "CSCI370"], ["CSCI274", "CSCI303", "CSCI341", "MATH213", "MATH225"],["CSCI274", "CSCI303", "CSCI341", "MATH213", "MATH225"],["CSCI274", "CSCI303", "CSCI341", "MATH213", "MATH225"]];;
+    loadElements(generatedSchedule);
 }
 
 // checks to see if result is a json file
