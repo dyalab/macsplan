c6f98eb5e26bac2a011ceb7aa8445da814b5f4ff
Michael Lesartre <mlesartre@mines.edu>
2018-06-16 15:11:26 -0600

    swapped order again

--- website_files/server.js
+++ website_files/server.js
@@ -29,7 +29,7 @@ app.get('/majorData', (req, res) =>{
 });
 
 app.get('/RPC2/', (req, res) =>{
-    client.methodCall('plan', [catalog, req.param('student_data')], function(error, value){
+    client.methodCall('plan', [req.param('student_data'), catalog], function(error, value){
 	if (error){
 	res.send(error);
 	}
