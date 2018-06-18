6d8ed3de01ffeedea92da917e2eb3f93a61433ad
Sebastian Cabrol <cabrol@mymail.mines.edu>
2018-06-18 16:51:49 -0600

    minor changes

--- website_files/sendToBackEnd.js
+++ website_files/sendToBackEnd.js
@@ -110,7 +110,7 @@ $("#generateButton").click(function(){
 
 	sTest = '{"taken" : [], "degree" : ["and", "CSCI101",  "MATH111", "CSCI261", "MATH112",  "MATH213", "CSCI262", "CSCI274", "CSCI341", "CSCI358", "MATH225", "CSCI306", "MATH332", "CSCI403", "CSCI406", "MATH201", "CSCI370", "CSCI400", "CSCI442"]}';
 	//console.log(JSON.stringify(inputValues));
-    if(alert("do you want to download the input values JSON files?")){
+    if(confirm("do you want to download the input values JSON files?")){
         plan(JSON.stringify(inputValues), function(result){loadResults(result)}, function(error){returnError(error)});
         var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(inputValues));
         var downloadAnchorNode = document.createElement('a');
